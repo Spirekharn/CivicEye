13f3b7ca355928183242cd7ab839d372e48d64fa
Spirekharn <swagotoutsab@gmail.com>
2026-04-06 04:22:08 +0600

    Role based access made

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+
+    ROLE_CHOICES = (
+        ('citizen', 'Citizen'),
+        ('surveyor', 'Surveyor'),
+        ('worker', 'Worker'),
+        ('technician', 'Technician'),
+        ('admin', 'Admin'),
+    )
+
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.username
