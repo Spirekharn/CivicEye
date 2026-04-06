ba410c5f6bd3c3d911800d05745db34c5926334a
Nabiha <nabihaafrin183@gmail.com>
2026-04-06 07:54:34 +0600

    tests for authentication system

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,18 +1,32 @@
 from django.test import TestCase
-
-# Create your tests here.
-from django.test import TestCase
+from django.urls import reverse
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-class UserTest(TestCase):
+class AuthTests(TestCase):
 
-    def test_create_user(self):
-        user = User.objects.create_user(
-            username='testuser',
-            password='testpass123',
-            role='citizen'
-        )
-        self.assertEqual(user.username, 'testuser')
-        self.assertEqual(user.role, 'citizen')
+    def test_register_user(self):
+        response = self.client.post(reverse('register'), {
+            'username': 'testuser',
+            'password': '12345'
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(User.objects.filter(username='testuser').exists())
+
+    def test_login_user(self):
+        User.objects.create_user(username='testuser', password='12345')
+
+        response = self.client.post(reverse('login'), {
+            'username': 'testuser',
+            'password': '12345'
+        })
+
+        self.assertEqual(response.status_code, 302)
+
+    def test_dashboard_access(self):
+        user = User.objects.create_user(username='testuser', password='12345')
+        self.client.login(username='testuser', password='12345')
+
+        response = self.client.get(reverse('dashboard'))
+        self.assertEqual(response.status_code, 200)
