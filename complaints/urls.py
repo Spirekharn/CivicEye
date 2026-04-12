d5cbcfd078f2c30be21d87204ac54cf37ad96cb4
SpireKharn <swagotoutsab@gmail.com>
2026-04-13 03:57:47 +0600

    SCRUM-38: Complaint form UI created

---
+++ complaints/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import create_complaint
+
+urlpatterns = [
+    path('create/', create_complaint, name='create_complaint'),
+    path('complaints/', include('complaints.urls')),
+]
