ea87d77498c55f4dda596cbb2a348ef3af13a545
Nabiha845 <nabihaafrin183@gmail.com>
2026-04-18 13:12:10 -0700

    S.testing

---
+++ selenium/SeleniumPython/test_homepage.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+driver.get("http://127.0.0.1:8000")
+
+# Check page loaded
+assert "CivicEye" in driver.title
+
+driver.quit()
