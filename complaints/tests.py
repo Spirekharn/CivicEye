64d0eb813936580ead123eb220078540e9895862
Nabiha845 <nabihaafrin183@gmail.com>
2026-04-26 01:28:56 +0600

    test

--- complaints/tests.py
+++ complaints/tests.py
@@ -8,3 +8,9 @@ class ComplaintListTest(TestCase):
     def test_list_view(self):
         response = self.client.get('/complaints/')
         self.assertEqual(response.status_code, 200)
+
+class ComplaintDetailTest(TestCase):
+
+    def test_detail_view(self):
+        response = self.client.get('/complaints/1/')
+        self.assertEqual(response.status_code, 200)
