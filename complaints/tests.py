ac0dbbd3b1bae1c4859660fbd9749ac9c02d6468
sujana120 <23101120@uap-bd.edu>
2026-04-25 22:18:33 +0600

    43 list test

--- complaints/tests.py
+++ complaints/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+
+class ComplaintListTest(TestCase):
+
+    def test_list_view(self):
+        response = self.client.get('/complaints/')
+        self.assertEqual(response.status_code, 200)
